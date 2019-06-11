1c08639704f3018475e1d07e9488d271c5cdc0ca
Motolola <motolola@icloud.com>
2019-06-11 14:55:00 +0100

    Initial commit.

---
+++ src/main/java/io/motolola/learning/codewars/CodewarsApplication.java
@@ -0,0 +1,13 @@
+package io.motolola.learning.codewars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodewarsApplication.class, args);
+	}
+
+}
